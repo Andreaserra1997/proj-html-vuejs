e5b1dad23d28192a635a0f137d15b822c4237a2f
Andreaserra1997 <serra.andrea1997@gmail.com>
2023-05-30 10:59:39 +0200

    add AppAboutUS

---
+++ src/components/AppAboutUs.vue
@@ -0,0 +1,77 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <div class="background-img">
+    <div class="container">
+      <div class="square-left">
+        <img src="/img/about.png" />
+      </div>
+      <div class="square-right">
+        <div class="about">
+          <div class="icon">
+            <img src="/img/dog-icon.png" />
+            <span>About us</span>
+          </div>
+          <div class="title">Welcome DogMilo Pets</div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the insudtry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled
+          </p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the insudtry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.background-img {
+  background-image: url(./../../public/img/dog-logo.png);
+  background-repeat: no-repeat;
+  background-position-x: right;
+  background-position-y: 20%;
+  background-size: 12%;
+}
+.container {
+  display: flex;
+  max-width: 1150px;
+  margin: auto;
+}
+.square-left {
+  width: 50%;
+  padding: 4rem 2rem 4rem 0;
+  img {
+    width: 80%;
+  }
+}
+.square-right {
+  width: 50%;
+  display: flex;
+  align-items: center;
+}
+.about {
+  .icon {
+    display: flex;
+    align-items: center;
+    span {
+      padding-left: 1rem;
+    }
+  }
+  .title {
+    padding-top: 2rem;
+    font-size: 2rem;
+  }
+  p {
+    padding-top: 2rem;
+  }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,15 @@
 <script>
 import AppJumbo from "./AppJumbo.vue";
+import AppAboutUs from "./AppAboutUs.vue";
+
 export default {
-  components: { AppJumbo },
+  components: { AppJumbo, AppAboutUs },
 };
 </script>
 
 <template>
   <AppJumbo />
+  <AppAboutUs />
 </template>
 
 <style lang="scss" scoped></style>
